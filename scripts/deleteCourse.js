e024c4619dfd148f0925687033cbe8a868161869
jwilson915 <164590100+jwilson915@users.noreply.github.com>
2025-02-23 11:35:06 -0500

    Update deleteCourse.js
    Put in POST Request to Delete Course

--- scripts/deleteCourse.js
+++ scripts/deleteCourse.js
@@ -1,5 +1,5 @@
- // Delete Course function
-      function deleteCourse() {
+// Delete Course function
+      async function deleteCourse() {
         let courseId = document.getElementById('course-id').value;
         let index = courses.findIndex(course => course.id === courseId);
 
@@ -8,7 +8,27 @@
           return;
         }
 
-        courses.splice(index, 1);
-        alert("Course Deleted.");
-        clearForm();
+// Put in Post Request to Delete Course
+
+        try{
+          let response = await fetch('/api/courses', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({id: courseId})
+          });
+
+          if (response.ok) {
+            courses.splice(index, 1);
+            localStorage.setItem('courses', JSON.stringify(courses));
+            alert("Course Deleted.");
+            clearForm();
+          } else {
+            alert("Failed to delete course.")
+          }
+        } catch (error){
+          console.error("Error:", error);
+          alert("Error occured when deleting course.")
+        }
       }
