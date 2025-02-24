61856ce7cec6009739bd1010d9918ad5a28a555f
Tomomi Hobara <tomomihobra@hotmail.com>
2025-02-24 11:12:20 -0600

    Updated viewCourses.js

--- scripts/viewCourse.js
+++ scripts/viewCourse.js
@@ -1,16 +1,39 @@
 // View Course function
-      function viewCourse() {
-        const courseList = document.getElementById('course-list');
-        courseList.innerHTML = '';
-        courses.forEach(course => {
-          const courseItem = document.createElement('div');
-          courseItem.innerHTML = `
-        <p> Name: ${course.name} </p>
-        <p> ID: ${course.id} </p>
-        <p> Subject: ${course.subject} </p>
-        <p> Credits: ${course.credits} </p>
-        <p> Description: ${course.description} </p>
-        `;
-          courseList.appendChild(courseItem);
-        });
-      }
+      
+// Function to get the userId from local storage
+function getUserId() {
+  return localStorage.getItem('userId');
+}
+
+async function viewCourse() {
+
+  const response = await fetch(
+    "https://merciful-spiral-heliotrope.glitch.me/api/courses/"
+  );
+  const courses = await response.json(); // Get all courses
+
+  const userId = getUserId();
+
+  // Filter courses to include only those created by the current user
+  const filteredCourses = courses.filter(course => course.createdBy === userId);
+
+  filteredCourses.sort((a, b) => a.courseId.localeCompare(b.courseId));  // Sort courses by courseId
+
+
+  const courseList = document.getElementById('course-list');
+  courseList.innerHTML = '';
+  filteredCourses.forEach(course => {
+    const courseItem = document.createElement('div');
+    courseItem.innerHTML = `
+      <p> Name: ${course.courseName} </p>
+      <p> ID: ${course.courseId} </p>
+      <p> Subject: ${course.subject} </p>
+      <p> Credits: ${course.credits} </p>
+      <p> Description: ${course.description} </p>
+      <p> Created By: ${course.createdBy} </p>
+      <br>
+      `;
+    courseList.appendChild(courseItem);
+  });
+}
+
