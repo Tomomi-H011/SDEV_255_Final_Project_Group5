06cb52b4329274d18a0b2791694d62a4069d7ddf
Tomomi Hobara <tomomihobra@hotmail.com>
2025-02-24 14:36:08 -0600

    Get available courses for the select options.

--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['student', 'teacher'], required: true },
-  enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
+  // enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
+  enrolledCourses: [{ type: String}]
 });
 
 module.exports = mongoose.model('User', UserSchema);
